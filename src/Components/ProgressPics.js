9ec109065efe0cb428cb5f47d15b1130cfb1d248
getfit-us <99081550+getfit-us@users.noreply.github.com>
2022-08-16 20:17:24 -0400

    updated profile page with dropzone

--- src/Components/ProgressPics.js
+++ src/Components/ProgressPics.js
@@ -1,10 +1,25 @@
 // component to display group of pictures ..
 import React from 'react'
+import useProfile from "../utils/useProfile";
+import { Grid } from '@mui/material';
 
 const ProgressPics = () => {
-  return (
-    <div>ProgressPics</div>
-  )
+    const { state } = useProfile();
+    const progressPics = state.profile.measurements.map(measurement => {
+
+
+    })
+
+
+    return (
+        <Grid container>
+            <Grid item>
+                <img src={`http://localhost:8000/avatar/${state.measurements.avatar}`} alt="" />
+            </Grid>
+        </Grid>
+
+
+    )
 }
 
 export default ProgressPics
