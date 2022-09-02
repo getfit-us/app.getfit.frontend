d1462fcb9d90da436733805ace719bb520ce0815
Chris <chris@getfit.us>
2022-09-02 14:34:22 -0400

    change pc

--- src/Components/SingleExerciseForm.js
+++ src/Components/SingleExerciseForm.js
@@ -1,14 +1,34 @@
 import { Button, Grid, InputAdornment, TextField } from "@mui/material";
 import React, { useState } from "react";
 
+//Returns individual Exercise Forms
+//need to fix add set functions
 
-
-//Returns individual Exercise Forms 
 
 const SingleExerciseForm = ({ addExercise }) => {
-    const [numOfSets, setNumOfSets] = useState(1);
+  const [numOfSets, setNumOfSets] = useState([1]);
+  const [addSet, setAddset] = useState(false);
 
-   
+  const exerciseForm = (
+    <>
+      <Grid item xs={2}>
+        <TextField type="input" variant="outlined" label="Set" value={numOfSets} />
+      </Grid>
+      <Grid item xs={4}>
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Weight"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">lb</InputAdornment>,
+          }}
+        />
+      </Grid>
+      <Grid item xs={4}>
+        <TextField type="text" variant="outlined" label="Reps" />
+      </Grid>
+    </>
+  );
 
   return addExercise.map((exercise) => {
     //going to output exercise form for each
@@ -17,37 +37,27 @@ const SingleExerciseForm = ({ addExercise }) => {
         <Grid item xs={12} key={exercise.id}>
           <h3>{exercise.name}</h3>
         </Grid>
-        <Grid item xs={2}>
-          <TextField type="input" variant="outlined" label="Set" value="1" />
-        </Grid>
-        <Grid item xs={4}>
-          <TextField
-            type="text"
-            variant="outlined"
-            label="Weight"
-            InputProps={{
-              endAdornment: <InputAdornment position="end">lb</InputAdornment>,
-            }}
-          />
-        </Grid>
-        <Grid item xs={4}>
-        <TextField
-            type="text"
-            variant="outlined"
-            label="Reps"
-          
-          />
-        </Grid>
-            {/* add sets dynamically here  */}
-{numOfSets.map }
-            
+      
+        {/* add sets dynamically here  */}
+        {addSet &&
+          numOfSets.map((set) => {
+            return (
+              {exerciseForm}
+            );
+          })}
+
         <Grid item>
-            <Button variant='outlined' sx={{borderRadius: 10}}
+          <Button
+            variant="outlined"
+            sx={{ borderRadius: 10 }}
             onClick={() => {
-                setNumOfSets(prev => prev + 1)
-                console.log(numOfSets)
+              setNumOfSets((prev) => prev + 1);
+              setAddset(true)
+              console.log(numOfSets);
             }}
-            >Add Set</Button>
+          >
+            Add Set
+          </Button>
         </Grid>
       </Grid>
     );
