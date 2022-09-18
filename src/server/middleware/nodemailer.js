fbf3c0032447ada695e04a14a3d2823752c4d6ea
getfit-us <99081550+getfit-us@users.noreply.github.com>
2022-09-18 08:31:48 -0400

    modify nodemailer

--- src/server/middleware/nodemailer.js
+++ src/server/middleware/nodemailer.js
@@ -1,20 +1,31 @@
 const nodemailer = require("nodemailer");
 
-
 const transporter = nodemailer.createTransport({
-    sendmail: true,
-    newline: 'unix',
-    path: '/usr/sbin/sendmail'
+  sendmail: true,
+  newline: "unix",
+  path: "/usr/sbin/sendmail",
+});
+
+const sendEmail = async (clientEmail, clientLink) => {
+  const result = await transporter.sendMail({
+    from: "verify@getfit.us",
+    to: clientEmail,
+    subject: "Verify Email Address",
+    html: `
+        <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Welcome to GetFit</title>
+</head>
+<body>
+  <h2>Please verify your email address </h2>
+  <a href=${clientLink}>Verify your email address</a>
+</body>
+</html>`,
   });
+  return result;
+};
 
-  const sendEmail =  async (clientEmail, clientLink) =>  {
-   const result =  await transporter.sendMail({
-        from: 'verify@getfit.us',
-        to: clientEmail,
-        subject: 'Verify Email Address',
-        text: `Please Click the link to verify your email address and activate your account ${clientLink}.`
-      });
-      return result;
-  }
-
-  module.exports = sendEmail;
+module.exports = sendEmail;
