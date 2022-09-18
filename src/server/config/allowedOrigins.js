5464a103468b11319c2cfdcaf99ef720aed87392
getfit-us <99081550+getfit-us@users.noreply.github.com>
2022-09-17 20:25:34 -0400

    changed origins

--- src/server/config/allowedOrigins.js
+++ src/server/config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
     
-    'http://localhost:3000',
-    'http://www.getfitness.org',
-    'http://getfitness.org',
-    'http://127.0.0.1:3000',
+    'http://localhost:8000',
+    'https://app.getfit.us',
+    'http://app.getfit.us',
+    'http://127.0.0.1:8000',
    
 
 ];
