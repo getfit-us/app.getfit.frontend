afa696fa5260b78d4012858228c8d80494d916b2
getfit-us <99081550+getfit-us@users.noreply.github.com>
2022-10-01 15:06:02 -0400

    moved backened to own directory

--- src/server/config/roles_list.js
+++
@@ -1,8 +0,0 @@
-const ROLES_LIST = {
-    "Admin": 10,
-    "Trainer": 5,
-    "Client": 2,
-    "User": 1
-}
-
-module.exports = ROLES_LIST
--- src/server/routes/password.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const passwordController = require('../controllers/passwordController');
-
-router.put('/', passwordController.handleUpdatePassword);
-
-module.exports = router;
